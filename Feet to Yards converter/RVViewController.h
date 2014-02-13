7d6d39eed1b05d3fa695c430f0abf8381187ffbb
Randy Varela <randyvarela@Randys-MacBook-Pro.local>
2014-02-13 16:37:30 -0500

    Initial Commit

---
+++ Feet to Yards converter/RVViewController.h
@@ -0,0 +1,13 @@
+//
+//  RVViewController.h
+//  Feet to Yards converter
+//
+//  Created by Randy Varela on 2/13/14.
+//  Copyright (c) 2014 Randy Varela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RVViewController : UIViewController
+
+@end
---
+++ Feet to Yards converterTests/Feet_to_Yards_converterTests.m
@@ -0,0 +1,34 @@
+//
+//  Feet_to_Yards_converterTests.m
+//  Feet to Yards converterTests
+//
+//  Created by Randy Varela on 2/13/14.
+//  Copyright (c) 2014 Randy Varela. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Feet_to_Yards_converterTests : XCTestCase
+
+@end
+
+@implementation Feet_to_Yards_converterTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
